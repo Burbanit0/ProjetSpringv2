14b4c49c36cad94f0d0a06c2b5f413d3e9d8c81b
BURBAN <burbanmatthieu@yahoo.fr>
2021-12-07 14:47:55 +0100

    Patch method

--- tse_kanban_back-steps_1_2_3/src/main/java/org/rygn/kanban/controller/TaskController.java
+++ tse_kanban_back-steps_1_2_3/src/main/java/org/rygn/kanban/controller/TaskController.java
@@ -6,7 +6,11 @@ import org.rygn.kanban.dao.TaskRepository;
 import org.rygn.kanban.domain.Task;
 import org.rygn.kanban.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +36,11 @@ public class TaskController {
 	@PatchMapping(path = "/tasks/{id}", consumes = "application/json-patch+json")
 	public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody JsonPatch patch) {
 		try {
-			Task task = 
+			Task task = taskRepository.findById(id).get();
+			task.setDevelopers(null);
+			return new ResponseEntity<Task>(taskRepository.save(task), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
